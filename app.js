772838cca76f9f1bd350d814002e732ca9ce3312
pinalef <tierralavista@gmail.com>
2017-10-05 10:49:15 -0300

    proyecto cipher

---
+++ app.js
@@ -0,0 +1,13 @@
+function cipher(){
+	var result ="";
+	var cod= 0;
+
+	palabra = palabra.toUpperCase();
+	for( var i = 0 ; palabra.length; i++){
+		cod= palabra.charCodeAt(i);
+		cod=((cod-65+35)%26)+26;
+		result+=String.fromCharCode(cod);
+	}
+	alert(result);
+}
+((variable-65-7+26)%26)+65;
